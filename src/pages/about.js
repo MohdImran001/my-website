0747bc7f8a7655d9f98feebd7a725c044a0ff725
Mohammad Imran <imrancrazy200@gmail.com>
2021-10-15 11:36:18 +0530

    feat: :sparkles: Improve about me page

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,9 @@ import {
   faLinkedinIn,
   faWhatsapp,
   faGithub,
+  faUbuntu,
 } from "@fortawesome/free-brands-svg-icons"
+import { faCode, faTerminal } from "@fortawesome/free-solid-svg-icons"
 
 import Layout from "layouts/layout"
 import Seo from "components/seo"
@@ -16,7 +18,7 @@ const AboutPage = () => (
   <Layout>
     <main className="bg-gray-50 text-gray-900">
       <div className="max-w-7xl mx-auto  pb-20 px-4 lg:px-8 text-center">
-        <div className="pt-10 md:pt-12 mx-auto text-center text-white">
+        <div className="pt-10 md:pt-12 mx-auto text-left text-white">
           <div className="max-w-7xl  rounded-lg text-white transform rotate-3">
             <div className=" max-w-7xl text-gray-900 transform -rotate-3 rounded-lg p-6 px-2 lg:p-12 lg:pb-4">
               <div className="">
@@ -65,13 +67,21 @@ const AboutPage = () => (
                   <br />
                   <p className="mt-5 space-x-4">
                     <a href="https://www.linkedin.com/in/mohdimran001/">
-                      <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
-                    </a>
-                    <a href="https://wa.me/919818582518/?text=Hi%20Imran,%20Lets%20do%20a%20project%20together!">
-                      <FontAwesomeIcon icon={faWhatsapp} size="2x" />
+                      <FontAwesomeIcon
+                        icon={faLinkedinIn}
+                        size="2x"
+                        color="#3B82F6"
+                      />
                     </a>
                     <a href="https://github.com/MohdImran001/">
                       <FontAwesomeIcon icon={faGithub} size="2x" />
+                    </a>
+                    <a href="https://wa.me/919818582518/?text=Hi%20Imran,%20Lets%20do%20a%20project%20together!">
+                      <FontAwesomeIcon
+                        icon={faWhatsapp}
+                        size="2x"
+                        color="#34D399"
+                      />
                     </a>
                   </p>
                 </p>
@@ -89,27 +99,63 @@ const AboutPage = () => (
                     Coding
                   </span>
                 </div>
-                <p className=" lg:p-6 lg:px-24 text-sm md:text-lg  font-medium leading-loose lg:leading-9 md:tracking-wider">
-                  I loves to write easy to read and maintainable code. I prefer
-                  the following tools to improve productivity.
+                <p className=" lg:p-6 lg:px-24 text-sm md:text-lg font-medium leading-loose lg:leading-9 md:tracking-wider">
+                  I use the following softwares to improve productivity.
                   {/* GitLens, TabNine, Moon theme, vs code, fish, ubuntu,  */}
-                  <div className="mt-5 space-x-4 text-left">
-                    <div>
-                      <a href="https://www.linkedin.com/in/mohdimran001/">
-                        <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
-                      </a>
-                    </div>
-                    <div>
-                      <a href="https://wa.me/919818582518/?text=Hi%20Imran,%20Lets%20do%20a%20project%20together!">
-                        <FontAwesomeIcon icon={faWhatsapp} size="2x" />
-                      </a>
-                    </div>
-                    <div>
-                      <a href="https://github.com/MohdImran001/">
-                        <FontAwesomeIcon icon={faGithub} size="2x" />
-                      </a>
-                    </div>
+                  <div className="mt-5 space-x-4">
+                    <FontAwesomeIcon icon={faCode} size="1x" /> <b>Editor - </b>
+                    VS Code with
+                    <a
+                      href="https://taniarascia.github.io/new-moon/"
+                      style={{
+                        textDecorationLine: "underline",
+                        textDecorationStyle: "wavy",
+                        textDecorationColor: "#EC4899",
+                      }}
+                    >
+                      New Moon Theme
+                    </a>
                   </div>
+                  <div className="mt-5 space-x-4">
+                    <FontAwesomeIcon icon={faTerminal} size="1x" />
+                    <b>Terminal - </b>
+                    <a
+                      href="https://fishshell.com/"
+                      style={{
+                        textDecorationLine: "underline",
+                        textDecorationStyle: "wavy",
+                        textDecorationColor: "#EC4899",
+                      }}
+                    >
+                      Fish
+                    </a>
+                  </div>
+                  <div className="mt-5 space-x-4">
+                    <FontAwesomeIcon icon={faTerminal} size="1x" />
+                    <b>Operating System - </b>
+                    <FontAwesomeIcon icon={faUbuntu} size="1x" /> Ubuntu
+                  </div>
+                </p>
+              </div>
+              <div className="mt-32">
+                <div className="pl-0 p-4 text-2xl font-bold md:p-8 md:text-5xl lg:text-5xl lg:pt-10 lg:pl-4">
+                  <span
+                    style={{
+                      textDecorationLine: "underline",
+                      textDecorationStyle: "wavy",
+                      textDecorationColor: "#34D399",
+                    }}
+                  >
+                    Reading List
+                  </span>
+                </div>
+                <p className="p-4 text-sm md:text-lg font-medium leading-loose lg:leading-9 md:tracking-wider">
+                  <a href="https://sre.google/sre-book/table-of-contents/">
+                    <img
+                      className="shadow-xl rounded-md"
+                      src="https://lh3.googleusercontent.com/JvM0JKKuZNJMWAC5iZPm4j-mdS9ORpZbpEWzg0zmJ0i2_xgIcju0OLXJ-zmnvz_GtFFGHe9qZ9Dz-6W0u5fRLFQaRlOI_hGzbetw"
+                    />
+                  </a>
                 </p>
               </div>
             </div>
